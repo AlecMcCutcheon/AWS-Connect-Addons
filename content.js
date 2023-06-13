c2cb5c5d56a88acb368a99711b070c9c6db2b35b
MixtapeXD <46252377+AlecMcCutcheon@users.noreply.github.com>
2023-06-13 11:51:45 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,13 @@
+let link;
+
+fetch('https://raw.githubusercontent.com/AlecMcCutcheon/AWS-Connect-Addons/Hosted-JS/Hosted%20JS/Dyn%20Link.JSON')
+  .then(response => response.text())
+  .then(text => {
+    link = JSON.parse(text).LINK;
+    const script = document.createElement('script');
+    script.src = link;
+    document.body.appendChild(script);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
